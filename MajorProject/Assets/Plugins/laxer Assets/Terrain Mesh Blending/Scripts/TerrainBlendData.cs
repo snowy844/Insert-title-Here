ad53e48d6564c5c3658e7342ae58f8678a683e6d
SNOW-FAMILY\Mark <snowy844@gmail.com>
2017-10-15 10:11:31 +1100

    deleted doubles

--- MajorProject/Assets/Plugins/laxer Assets/Terrain Mesh Blending/Scripts/TerrainBlendData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TerrainBlendData : ScriptableObject {
-	[SerializeField]
-	public Texture2D blendmap;
-	public Texture2D blendnormalmap;
-
-}
